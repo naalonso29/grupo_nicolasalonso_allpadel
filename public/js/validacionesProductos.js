76de83eb243c3b27be5ed8c7f2a502ec27f27de4
naalonso29 <naalonso29@gmail.com>
2022-04-12 18:41:28 -0300

    correcciones

--- public/js/validacionesProductos.js
+++ public/js/validacionesProductos.js
@@ -24,6 +24,39 @@ document.getElementById("file").onchange = function(e) {
     };
   }
 
+
+  document.getElementById("fileMarca").onchange = function(e) {
+    // Creamos el objeto de la clase FileReader
+    let reader = new FileReader();
+  
+    // Leemos el archivo subido y se lo pasamos a nuestro fileReader
+    reader.readAsDataURL(e.target.files[0]);
+  
+    // Le decimos que cuando este listo ejecute el código interno
+    reader.onload = function(){
+        let preview = document.getElementById('previewMarca')
+  
+        preview.src = reader.result;
+
+    };
+  }
+
+  document.getElementById("fileColor").onchange = function(e) {
+    // Creamos el objeto de la clase FileReader
+    let reader = new FileReader();
+  
+    // Leemos el archivo subido y se lo pasamos a nuestro fileReader
+    reader.readAsDataURL(e.target.files[0]);
+  
+    // Le decimos que cuando este listo ejecute el código interno
+    reader.onload = function(){
+        let preview = document.getElementById('previewColor')
+  
+        preview.src = reader.result;
+
+    };
+  }  
+
 formulario.addEventListener("submit", (e) => {
 
     errores = []
